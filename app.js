3e9b04df7ea0467bf45521a75db84b6bb17cd40f
Caleb Faught <faughtcj@gmail.com>
2015-10-19 21:16:05 -0500

    fixed port declaration

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 
 //server listening on port 80
-app.listen(process.env.PORT || 80);
-
+var port = process.env.PORT || 80;
 app.listen(port, function() {
   console.log('The server app is runing on port: ' + port);
 });
