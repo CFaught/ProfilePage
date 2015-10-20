7dc25855ad26fb675b93f670d1dfae0b05734b73
Caleb Faught <faughtcj@gmail.com>
2015-10-19 21:02:34 -0500

    added a console log showing that the server is running within app.js

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var app = express();
 
+//make express look in the public directory for resources
 app.use(express.static(__dirname + '/public'));
 
+//server listening on port 80
 app.listen(process.env.PORT || 80);
+
+app.listen(port, function() {
+  console.log('The server app is runing on port: ' + port);
+});
